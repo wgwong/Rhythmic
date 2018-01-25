bbe346f8a4a29098919733e9ae1550434718e0da
unknown <wgwong@DESKTOP-O2FS9QG.mit.edu>
2018-01-25 03:48:45 -0500

    preprocessed audio that generates a beatmap which changes the positions of the balls in the visualizer as the audio plays; working preprocessed (with fft) beat detection map creator

--- Assets/Scripts/AudioParser.cs
+++ Assets/Scripts/AudioParser.cs
@@ -3,26 +3,124 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioParser : MonoBehaviour {
+/*
+ * DEFUNCT , please refer to AudioReader.cs for updates
+ * 
+ */
+public class AudioParser : MonoBehaviour
+{
     AudioSource audioSource;
 
+    public const int numOfSamples = 512;
+    public float[] threshold;
+
+    public float[] freqData;
+    public float[] band;
+
+    public GameObject[] g;
+
+
+    public float[] highVals; //debug
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+        freqData = new float[numOfSamples];
+        int n = freqData.Length;
+        int k = 0;
+        for (int j = 0; j < freqData.Length; j++)
+        {
+            n = n / 2;
+            if (n <= 0)
+                break;
+            k++;
+        }
+
+        band = new float[k + 1];
+        g = new GameObject[k + 1];
+        threshold = new float[k + 1];
+        threshold[0] = 0;
+        threshold[1] = 12;
+        threshold[2] = 10.5f;
+        threshold[3] = 6;
+        threshold[4] = 6.5f;
+        threshold[5] = 4.5f;
+        threshold[6] = 2;
+        threshold[7] = 1;
+        threshold[8] = 0.5f;
+        threshold[9] = 0;
+
+        highVals = new float[k + 1]; //debug
+
+        for (int i = 0; i < band.Length; i++)
+        {
+            band[i] = 0;
+            g[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            g[i].GetComponent<Renderer>().material.SetColor("_Color", Color.cyan);
+            g[i].transform.position = new Vector3(i, 0, 0);
+        }
+
         audioSource = GetComponent<AudioSource>();
         audioSource.Play();
     }
 
-	// Update is called once per frame
-	void Update () {
-        float[] spectrum = new float[256];
-        audioSource.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+    private void check()
+    {
+        audioSource.GetSpectrumData(freqData, 0, FFTWindow.Rectangular);
 
-        for (int i = 1; i < spectrum.Length - 1; i++)
+        int k = 0;
+        int crossover = 2;
+        for (int i = 0; i < freqData.Length; i++)
         {
-            //Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
-            Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
-            //Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
-            //Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
+            float d = freqData[i];
+            float b = band[k];
+
+            // find the max as the peak value in that frequency band.
+            band[k] = (d > b) ? d : b;
+
+            if (i > (crossover - 3))
+            {
+                k++;
+                crossover *= 2; // frequency crossover point for each band.
+                float height = band[k] * 32;
+                Vector3 tmp = new Vector3(g[k].transform.position.x, height, g[k].transform.position.z);
+                g[k].transform.position = tmp;
+                band[k] = 0;
+                if (height > highVals[k])
+                {
+                    highVals[k] = height; //debug
+                }
+            }
         }
+    }
+
+    private void display()
+    {
+        for (int i = 0; i < g.Length; i++)
+        {
+            if (g[i].transform.position.y > threshold[i])
+            {
+                g[i].SetActive(true);
+            }
+            else
+            {
+                g[i].SetActive(false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        check();
+        //display ();
+
+        String s = ""; //debug
+        for (int i = 0; i < highVals.Length; i++)
+        {
+            float val = highVals[i]; //debug
+            s = s + i + ": " + highVals[i] + " "; //debug
+        }
+        //Debug.Log (s); //debug
     }
 }
---
+++ Assets/Scripts/AudioReader.cs
@@ -0,0 +1,175 @@
+﻿using UnityEngine;
+using System.Collections;
+using Lomont;
+using System;
+using System.Collections.Generic;
+
+public class AudioReader : MonoBehaviour {
+    AudioSource audioSource;
+    LomontFFT fft;
+	AudioClip clip;
+
+
+    float secondInMilliseconds = 1000;
+    float samplingInterval = 20; //in ms
+    int samplingWindowSize = 512;
+    int steps;
+
+
+    public GameObject[] g;
+    public float[] band;
+
+    Dictionary<int, Vector3[]> positions;
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+        clip = audioSource.clip;
+        fft = new LomontFFT();
+		float[] samples = new float[clip.samples * clip.channels];
+		clip.GetData (samples, 0);
+        //convert float to double
+        double[] samplesD = new double[samples.Length];
+        for (int i = 0; i < samplesD.Length; i++) {
+            samplesD[i] = (double)samples[i];
+        }
+
+        Debug.Log("clip samples: " + clip.samples); //debug
+        Debug.Log("clip channels: " + clip.channels); //debug
+        Debug.Log("clip duration: " + clip.length); //debug
+
+        
+        steps = Mathf.RoundToInt(samplingInterval / secondInMilliseconds * clip.frequency);
+        Debug.Log("steps: " + steps); //debug
+
+
+
+
+        
+        int n = samplingWindowSize;
+        int k = 0;
+        for (int j = 0; j < samplingWindowSize; j++)
+        {
+            n = n / 2;
+            if (n <= 0)
+                break;
+            k++;
+        }
+        band = new float[k + 1];
+        g = new GameObject[k + 1];
+        positions = new Dictionary<int, Vector3[]>();
+
+        for (int i = 0; i < band.Length; i++)
+        {
+            band[i] = 0;
+            g[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            g[i].GetComponent<Renderer>().material.SetColor("_Color", Color.cyan);
+            g[i].transform.position = new Vector3(i, 0, 0);
+        }
+
+
+
+        for (int i = 0; i < samplesD.Length; i = i + steps)
+        {
+            double[] samplingWindow = new double[samplingWindowSize];
+            if (i > samplingWindowSize)
+            {
+                Array.Copy(samplesD, i - samplingWindowSize / 2, samplingWindow, 0, samplingWindowSize);
+            } else //i = 0
+            {
+                Array.Copy(samplesD, i, samplingWindow, 0, samplingWindowSize);
+            }
+
+            fft.FFT(samplingWindow, true);
+
+            //convert double to float
+            float[] samplingWindowF = new float[samplingWindow.Length];
+            for (int j = 0; j < samplingWindow.Length; j++)
+            {
+                samplingWindowF[j] = (float)samplingWindow[j];
+            }
+
+            checkWindow(i, samplingWindowF);
+
+            /*
+            if (i > 70000)
+            {
+                break;
+            }*/
+        }
+
+
+
+        /*
+        using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\wgwong\Desktop\out2.txt"))
+        {
+            foreach (var entry in positions)
+            {
+                file.WriteLine("position: " + entry.Key);
+                for (int i = 0; i < entry.Value.Length; i++) {
+                    var element = entry.Value[i];
+                    file.WriteLine("element[" + i + "]: " + element.x + ", " + element.y + ", " + element.z);
+                }
+                file.WriteLine("\n");
+            }
+        }*/
+
+        Debug.Log("positions keys: " + positions.Keys.ToString());
+
+        audioSource.Play();
+        
+	}
+	
+    void checkWindow(int pos, float[] freqData)
+    {
+        int k = 0;
+        int crossover = 2;
+        for (int i = 0; i < freqData.Length; i++)
+        {
+            float d = freqData[i];
+            float b = band[k];
+
+            // find the max as the peak value in that frequency band.
+            band[k] = (d > b) ? d : b;
+
+            if (i > (crossover - 3))
+            {
+                k++;
+                crossover *= 2; // frequency crossover point for each band.
+                float height = band[k] * 32;
+                Vector3 tmp = new Vector3(g[k].transform.position.x, height, g[k].transform.position.z);
+                g[k].transform.position = tmp;
+                band[k] = 0;
+
+                if (positions.ContainsKey(pos))
+                {
+                    positions[pos][k] = tmp;
+                } else
+                {
+                    positions.Add(pos, new Vector3[g.Length]);
+                }
+            }
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        int currentSample = audioSource.timeSamples;
+        int pos = Mathf.FloorToInt( ((float) currentSample) / steps);
+        int curStep = pos * steps * clip.channels;
+        
+        /*
+        Debug.Log("currentSample: " + currentSample);
+        Debug.Log("pos: " + pos);
+        Debug.Log("curStep: " + curStep);
+        */
+
+        Vector3[] displayPositions = positions[curStep];
+
+        for (int i = 0; i < displayPositions.Length; i++)
+        {
+            g[i].transform.position = displayPositions[i];
+        }
+    }
+}
